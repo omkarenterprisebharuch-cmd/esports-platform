5203a253eeac9ab2cffb41285a5903bfedbd33d3
vaibhav lohiya <vaibhav60080@gmail.com>
2026-01-02 00:22:05 +0530

    chore: hide sensitive code from public repository

--- src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export { useOnDemandFetch, useLazyReveal } from './useOnDemandFetch';
-export { useUserCache, UserCacheProvider } from './useUserCache';
-export { useRegistrationCache, RegistrationCacheProvider, clearRegistrationCache } from './useRegistrationCache';
-export { useApi, useMutation, useInfiniteApi, clearCache } from './useApi';
-export { useIdleTimeout, getSessionRemainingTime, hasActiveSession, clearSessionData } from './useIdleTimeout';
-export type { RetryConfig } from './useApi';
